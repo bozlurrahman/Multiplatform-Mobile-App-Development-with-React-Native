ebbd4704560309677aebed98186c5d8b090c83f2
Bozlur Rahman <bozlurrahman.cmt@gmail.com>
2021-01-23 16:17:24 +0600

    React Native Navigation Part 3

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,21 +1,59 @@
 import React, { Component } from 'react';
-import { Button, View, Text } from 'react-native';
+import { Button, Text, ScrollView, View } from 'react-native';
+import { Card } from 'react-native-elements';
+import { DISHES } from '../shared/dishes';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
+
+function RenderItem(props) {
+    
+    const item = props.item;
+    
+    if (item != null) {
+        return(
+            <Card>
+                <Card.Title>{item.name}</Card.Title>
+                {/* <Card.Image source={require( './'+ DISHES.filter(function(dis){return dish.id == dis.id})[0].image )} /> */}
+                {/* <Card.Image source={ img_path } /> */}
+                <Card.Image source={require('./images/uthappizza.png')} />
+                <Text style={{margin: 10}}>
+                    {item.description}
+                </Text>        
+            </Card>
+        );
+    }
+    else {
+        return(<View></View>);
+    }
+}
 
 class Home extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+          dishes: DISHES,
+          promotions: PROMOTIONS,
+          leaders: LEADERS
+        };
+    }
+
     static navigationOptions = {
-        title: 'Home'
+        title: 'Home',
     };
 
     render() {
         
         return(
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Text>Lorem</Text>
+            <ScrollView>
+                <RenderItem item={this.state.dishes.filter((dish) => dish.featured)[0]} />
+                <RenderItem item={this.state.promotions.filter((promo) => promo.featured)[0]} />
+                <RenderItem item={this.state.leaders.filter((leader) => leader.featured)[0]} />
                 <Button onPress={() => this.props.navigation.toggleDrawer()} title="Toggle Drawer" />
-            </View>
+            </ScrollView>
         );
     }
+
 }
 
 export default Home;
