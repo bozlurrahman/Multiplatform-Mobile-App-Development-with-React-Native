3fdf0725d86150e06022329f032289b7458b53ba
Bozlur Rahman <bozlurrahman.cmt@gmail.com>
2021-01-27 10:46:08 +0600

    Swipeout and Deleting Favorites

--- components/FavoriteComponent.js
+++ components/FavoriteComponent.js
@@ -1,26 +1,51 @@
 import React, { Component } from 'react';
-import { FlatList, View, Text } from 'react-native';
+import { Animated, StyleSheet, FlatList, View, Text } from 'react-native';
 import {Avatar, Card, ListItem} from 'react-native-elements';
 import { connect } from 'react-redux';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { deleteFavorite } from '../redux/ActionCreators';
+import Swipeable from 'react-native-gesture-handler/Swipeable';
+import { RectButton } from 'react-native-gesture-handler';
+
+const AnimatedText = Animated.createAnimatedComponent(Text);
 
 const mapStateToProps = state => {
     return {
       dishes: state.dishes,
       favorites: state.favorites
     }
-  }
+}
+
+const mapDispatchToProps = dispatch => ({
+    deleteFavorite: (dishId) => dispatch(deleteFavorite(dishId))
+})
 
 class Favorites extends Component {
 
     render() {
 
         const { navigate } = this.props.navigation;
-        
+
         const renderMenuItem = ({item, index}) => {
-            console.log('lorem: ', item);
+            const rightButton = (progress, dragX) => {
+                const scale = dragX.interpolate({
+                  inputRange: [-80, 0],
+                  outputRange: [1, 0],
+                  extrapolate: 'clamp',
+                });
+                return (
+                  <RectButton style={styles.rightAction} 
+                    // onPress={this.close}
+                    onPress= {() => this.props.deleteFavorite(item.id)}
+                    >
+                      <AnimatedText color="#fff" style={[styles.actionIcon, { transform: [{ scale }] }]}>Delete</AnimatedText>
+                  </RectButton>
+                );
+            };
+
             return (
+                <Swipeable renderRightActions={rightButton}>
                 <ListItem key={item.id} onPress={() => navigate('Dishdetail', { dishId: item.id })}>
                     <Avatar source={{ uri: ( baseUrl + item.image )}} />
                     <ListItem.Content>
@@ -28,6 +53,7 @@ class Favorites extends Component {
                         <ListItem.Subtitle>{item.description}</ListItem.Subtitle>
                     </ListItem.Content>
                 </ListItem>
+                </Swipeable>
             );
         };
 
@@ -55,5 +81,18 @@ class Favorites extends Component {
     }
 }
 
+const styles = StyleSheet.create({
+    actionIcon: {
+      width: 80,
+      marginHorizontal: 10,
+    },
+    rightAction: {
+      alignItems: 'center',
+      flexDirection: 'row',
+      backgroundColor: '#dd2c00',
+      flex: 1,
+      justifyContent: 'flex-end',
+    },
+});
 
-export default connect(mapStateToProps)(Favorites);
+export default connect(mapStateToProps, mapDispatchToProps)(Favorites);
